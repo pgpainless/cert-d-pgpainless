6a16d0cff554cc9eaf2b3a306904cb1d44fdc49d
Paul Schaub <vanitasvitae@fsfe.org>
2022-08-08 15:04:22 +0200

    Implement armor option for export command

--- pgpainless-cert-d-cli/src/main/java/pgp/cert_d/cli/commands/Export.java
+++ pgpainless-cert-d-cli/src/main/java/pgp/cert_d/cli/commands/Export.java
@@ -4,6 +4,7 @@
 
 package pgp.cert_d.cli.commands;
 
+import org.bouncycastle.bcpg.ArmoredOutputStream;
 import org.bouncycastle.util.io.Streams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,27 +14,39 @@ import picocli.CommandLine;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Iterator;
 
 @CommandLine.Command(name = "export",
         resourceBundle = "msg_export")
 public class Export implements Runnable {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Get.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Export.class);
+
+    @CommandLine.Option(names = {"-a", "--armor"})
+    boolean armor = false;
 
     @Override
     public void run() {
         Iterator<Certificate> certificates = PGPCertDCli.getCertificateDirectory()
                 .getCertificates();
+        OutputStream out = armor ? new ArmoredOutputStream(System.out) : System.out;
         while (certificates.hasNext()) {
             try {
                 Certificate certificate = certificates.next();
                 InputStream inputStream = certificate.getInputStream();
-                Streams.pipeAll(inputStream, System.out);
+                Streams.pipeAll(inputStream, out);
                 inputStream.close();
             } catch (IOException e) {
                 LOGGER.error("IO Error", e);
                 System.exit(-1);
+            }
+        }
+        if (armor) {
+            try {
+                out.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
     }
